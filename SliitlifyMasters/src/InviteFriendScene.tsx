cac9346846b64f04d1a06860654479f8139df4f6
SonyRathnayake <31077931+SonyRathnayake@users.noreply.github.com>
2021-10-03 02:07:33 +0530

    Student profile added
    firebase dependencies added to package.json

--- SliitlifyMasters/src/InviteFriendScene.tsx
+++ SliitlifyMasters/src/InviteFriendScene.tsx
@@ -8,6 +8,8 @@ import {
   SafeAreaView,
   Platform,
   StatusBar,
+  Dimensions,
+  Animated,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { DrawerNavigationProp } from '@react-navigation/drawer';
@@ -16,7 +18,8 @@ import { AppImages } from '../res';
 import Config from './Config';
 
 interface Props {}
-
+const deviceWidth = Dimensions.get('screen').width;
+const deviceHeight = Dimensions.get('screen').height;
 const InviteFriendScene: React.FC<Props> = () => {
   const navigation = useNavigation<DrawerNavigationProp<{}>>();
 
@@ -38,26 +41,31 @@ const InviteFriendScene: React.FC<Props> = () => {
           <Icon name="menu" size={25} color="black" />
         </Pressable>
       </View>
-      <Image
-        style={styles.image}
-        source={AppImages.inviteImage}
-        resizeMode="cover"
-      />
-      <Text style={styles.title}>Invite Your Friends</Text>
-      <Text style={styles.subTitle}>
-        Are you one of those who makes everything{'\n'} at the last moment?
-      </Text>
+
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Pressable
-          style={({ pressed }) => [
-            styles.button,
-            { opacity: !Config.isAndroid && pressed ? 0.4 : 1 },
-          ]}
-          android_ripple={{ color: 'grey' }}
-        >
-          <Icon name="share" size={25} color="white" />
-          <Text style={styles.buttonText}>Share</Text>
-        </Pressable>
+        <Animated.View style={[styles.drawerAvatarStyle, styles.avatarShadow]}>
+          <Animated.Image
+            style={[styles.drawerAvatarStyle]}
+            source={AppImages.userImage}
+          />
+        </Animated.View>
+        <Text style={styles.title}>Sonal Rathnayake</Text>
+        <Text style={styles.subTitle}>ms21911958@mysliit.lk</Text>
+      </View>
+      <View style={styles.cardView}>
+        <Text style={styles.title}>Student Number</Text>
+        <Text style={styles.subTitle}>MS21911958</Text>
+        <Text />
+        <Text style={styles.title}>Masters Programme</Text>
+        <Text style={styles.subTitle}>
+          MSc. Enterprise Application Developments
+        </Text>
+        <Text />
+        <Text style={styles.title}>Cumulative GPA</Text>
+        <Text style={styles.subTitle}>3.6</Text>
+        <Text />
+        <Text style={styles.title}>Current Year / Current Semester</Text>
+        <Text style={styles.subTitle}>1/2</Text>
       </View>
     </SafeAreaView>
   );
@@ -68,6 +76,16 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     width: undefined,
     height: 320,
+  },
+  avatarShadow: {
+    elevation: 16,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+    shadowOpacity: 0.44,
+    shadowRadius: 10.32,
   },
   title: {
     fontSize: 20,
@@ -92,11 +110,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  drawerAvatarStyle: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+  },
   buttonText: {
     color: 'white',
     fontWeight: '500',
     textAlign: 'center',
     padding: 4,
+  },
+  cardView: {
+    width: deviceWidth - 32,
+    height: deviceHeight - 350,
+    alignSelf: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    borderRadius: 10,
+    padding: 25,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: '10%',
+    backgroundColor: 'white',
   },
 });
 
